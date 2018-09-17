dc55d4919f77a38326a0719b1eb7dc9442668f18
Harkins, Richard <richard.harkins@monsanto.com>
2018-09-17 12:55:20 -0500

    Adding webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './lib/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{ test: /\.js$/, use: 'babel-loader' }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, 'examples/index.html')
+    })
+  ],
+  watch: true
+};
