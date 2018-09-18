5644b17b1779a64dc03c5e0583139bc005f9f02d
Ricky Harkins <rickyharkins@gmail.com>
2018-09-17 21:19:49 -0500

    Changing to UMD

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,28 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './lib/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
+    filename: 'index.js',
+    library: '',
+    libraryTarget: 'umd'
   },
   module: {
-    rules: [{ test: /\.js$/, use: 'babel-loader' }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [require("@babel/plugin-transform-modules-commonjs")],
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
